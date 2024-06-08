763ce7743d740e42dc449f65af8340eaf82a2b1a
bcimbleris <bcimbleris@gmail.com>
2024-06-08 17:13:44 -0300

    cria comentarios para todas as funcoes do codigo

--- client.c
+++ client.c
@@ -8,13 +8,13 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <sys/types.h>
-
+// struct correspondente ao cliente
 typedef struct {
     int socket;
     int choice;
     struct sockaddr_storage storage;
 } ClientInfo;
-
+// testa se os argumentos foram passados da maneira correta no terminal
 void usage(int argc, char **argv) {
     printf("usage: %s <ipv4|ipv6> <server IP> <server port>\n", argv[0]);
     printf("example: %s ipv4 127.0.0.1 51511\n", argv[0]);
@@ -22,67 +22,78 @@ void usage(int argc, char **argv) {
 }
 
 #define BUFSZ 1024
-
+// funcao que cria o menu principal para o cliente
 void menu() {
     printf("0 - Sair\n");
     printf("1 - Senhor dos Aneis\n");
     printf("2 - Blade Runner 2049\n");
     printf("3 - Mad Max: Fury Road\n");
-    printf("Escolha: ");
 }
-
+// verifica se foram passadas as quantidades certas de argumentos
 int main(int argc, char **argv) {
     if (argc < 4) {
         usage(argc, argv);
     }
 
     struct sockaddr_storage storage;
+    // retorna 0 quando funciona,recebe endereço do servidor, porto e
+    // ponteiro para o sockaddr_storage que vai ser inicializado
     if (0 != addrparse(argv[1], argv[2], argv[3], &storage)) {
         usage(argc, argv);
     }
-
+    // abre socket para conexão UDP utilizando SOCK_DGRAM
     int s = socket(storage.ss_family, SOCK_DGRAM, 0);
+    // checando se houve algum erro ao abrir socket
     if (s == -1) {
         logexit("socket");
     }
-
+    // uma interface que instancia um ponteiro e é feito o cast para o tipo
+    // e coloca dentro da variável
     struct sockaddr *addr = (struct sockaddr *)(&storage);
     char addrstr[BUFSZ];
     addrtostr(addr, addrstr, BUFSZ);
-
+    
     while (1) {
+        //cria o objeto do tipo ClientInfo e preenche seus membros 
         ClientInfo clientInfo;
         clientInfo.socket = s;
         clientInfo.storage = storage;
-
+        //chama a funcao de menu dentro do loop para que nao seja necessario utilizar goto e label
         menu();
         int clientResponse;
+        //recebe qual opcao de filme o cliente quer
         scanf("%d", &clientResponse);
 
         if (clientResponse == 0) {
             break;
         }
-
+        //membro que determina as frases que serao disponibilizadas para o cliente
         clientInfo.choice = clientResponse;
-
+        //envia a struct correspondente ao cliente com as informações relevantes através do socket UDP inicializado anteriormente
         size_t count = sendto(s, &clientInfo, sizeof(clientInfo), 0, addr,
                               sizeof(storage));
+        //checa se houve algum problema
         if (count != sizeof(clientInfo)) {
             logexit("sendto");
         }
-
+        //comunicacao entre cliente e servidor
         char buf[BUFSZ];
+        //inicializa o buffer com 0
         memset(buf, 0, BUFSZ);
         socklen_t addrlen = sizeof(storage);
+        //foi utilizado um loop para contar as 5 frases que devem ser recebidas pelo cliente antes de retornar ao menu inicial
         for (int i = 0; i < 5; i++) {
+            //recebe a resposta do servidor no socket s, e coloca esse dado no buff
             count = recvfrom(s, buf, BUFSZ - 1, 0, addr, &addrlen);
             if (count == -1) {
                 logexit("recvfrom");
             }
-            buf[count] = '\0'; // Null-terminate the received data
+            buf[count] = '\0'; // coloca ao final da recepcao da mensagem para sinalizar o final de uma frase
+            //printa a frase recebida pelo servidor
             printf("Frase %d: %s\n", i + 1, buf);
         }
     }
+    //fecha a conexao
     close(s);
     return 0;
 }
--- server-mt.c
+++ server-mt.c
@@ -10,19 +10,19 @@
 #include <sys/types.h>
 
 #define BUFSZ 1024
-
+// funcao que verifica se o programa foi utilizado de maneira correta
 void usage(int argc, char **argv) {
     printf("usage: %s <ipv4|ipv6> <server port>\n", argv[0]);
     printf("example: %s ipv4 51511\n", argv[0]);
     exit(EXIT_FAILURE);
 }
-
+// cria a struct para os dados do clietne
 typedef struct {
     int socket;
     int choice;
     struct sockaddr_storage storage;
 } ClientInfo;
-
+// abaixo foram inicializados arrays com as frases dos filmes escolhidos
 const char *lotr_quotes[] = {
     "Even the smallest person can change the course of the future.",
     "There is only one Lord of the Ring, only one who can bend it to his will. "
@@ -42,24 +42,33 @@ const char *madmax_quotes[] = {"Oh what a day, what a lovely day!",
                                "My name is Max. My world is fire and blood.",
                                "I live, I die. I live again."};
 
+// uma variavel mutex foi a melhor maneira encontrada atraves de pesquisa para
+// conseguir mostrar a quantidade de clientes ativos a cada 4 segundos.
 pthread_mutex_t client_count_mutex = PTHREAD_MUTEX_INITIALIZER;
 int client_count = 0;
-
 void *monitor_clients(void *arg) {
     while (1) {
         sleep(4);
+        // utiliza-se uma funcao que previne que outras threads atrapalhem a
+        // contagem de clientes ativos em um determinado instante. Essa funcao
+        // trava a quantidade de clientes antes de mostrar
         pthread_mutex_lock(&client_count_mutex);
         printf("Clientes: %d\n", client_count);
+        // essa funcao libera a contagem para mudar
         pthread_mutex_unlock(&client_count_mutex);
     }
     return NULL;
 }
-
+// funcao responsavel por enviar as frases para o cliente
 void send_quotes(int sock, const char *quotes[], struct sockaddr *caddr,
                  socklen_t caddrlen) {
+    // buffer para o envio de informacoes para o cliente
     char send_buf[BUFSZ];
     for (int i = 0; i < 5; i++) {
+        // escreve no buffer as frases dos filmes
         snprintf(send_buf, BUFSZ, "%s\n", quotes[i]);
+        // envia para o cliente atraves do socket a frase correspondente à sua
+        // escolha de filme
         size_t send_count =
             sendto(sock, send_buf, strlen(send_buf), 0, caddr, caddrlen);
         if (send_count != strlen(send_buf)) {
@@ -68,16 +77,18 @@ void send_quotes(int sock, const char *quotes[], struct sockaddr *caddr,
         sleep(3);
     }
 }
-
+// inicializa a thread para que possam ser possiveis varios clientes
+// simultaneamente
 void *client_thread(void *data) {
     ClientInfo *clientInfo = (ClientInfo *)data;
     struct sockaddr *caddr = (struct sockaddr *)(&clientInfo->storage);
     socklen_t caddrlen = sizeof(clientInfo->storage);
 
     pthread_mutex_lock(&client_count_mutex);
+    // incrementa a contagem de clientes ativos
     client_count++;
     pthread_mutex_unlock(&client_count_mutex);
-
+    //chama a funcao send_quotes a partir da escolha feita pelo cliente
     if (clientInfo->choice == 1) {
         send_quotes(clientInfo->socket, lotr_quotes, caddr, caddrlen);
     } else if (clientInfo->choice == 2) {
@@ -87,14 +98,17 @@ void *client_thread(void *data) {
     }
 
     pthread_mutex_lock(&client_count_mutex);
+    // decrementa a contagem de clientes ativos
     client_count--;
     pthread_mutex_unlock(&client_count_mutex);
-
+    // libera a memoria relativa as informacoes do cliente apos finalizacao do
+    // envio e finaliza a thread
     free(clientInfo);
     pthread_exit(EXIT_SUCCESS);
 }
 
 int main(int argc, char **argv) {
+    // checa se o programa esta sendo utilizado corretamente
     if (argc < 3) {
         usage(argc, argv);
     }
@@ -103,49 +117,65 @@ int main(int argc, char **argv) {
     if (0 != server_sockaddr_init(argv[1], argv[2], &storage)) {
         usage(argc, argv);
     }
-
+    // inicializa o socket UDP
     int s = socket(storage.ss_family, SOCK_DGRAM, 0);
     if (s == -1) {
         logexit("socket");
     }
 
     int enable = 1;
+    // verifica se houve algum probelma com o socket
     if (0 != setsockopt(s, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int))) {
         logexit("setsockopt");
     }
 
     struct sockaddr *addr = (struct sockaddr *)(&storage);
+    // realiza o bind de um nome para um socket
     if (0 != bind(s, addr, sizeof(storage))) {
         logexit("bind");
     }
 
     char addrstr[BUFSZ];
     addrtostr(addr, addrstr, BUFSZ);
-
+    // Declara uma variável do tipo pthread_t para identificar a thread de
+    // monitoramento.
     pthread_t monitor_tid;
+    // Cria uma nova thread de monitoramento usando pthread_create, utiliza o
+    // endereço da variável monitor_tid onde o ID da nova thread será
+    // armazenado, função que a nova thread irá executar e flag que nao e
+    // utilizada
     pthread_create(&monitor_tid, NULL, monitor_clients, NULL);
+    // Desanexa a nova thread de monitoramento da thread principal, permite que
+    // a nova thread continue a execução independentemente da thread principal.
     pthread_detach(monitor_tid);
-
+    // o loop abaixo aguarda a chegada de mensagens do cliente
     while (1) {
+        // aloca memoria
         ClientInfo *clientInfo = malloc(sizeof(*clientInfo));
+        // testa se a alocacao de memoria foi bem sucedida
         if (!clientInfo) {
             logexit("malloc");
         }
-
+        // tamanho do endereco do cliente
         socklen_t caddrlen = sizeof(clientInfo->storage);
+        // configura o socket
         clientInfo->socket = s;
+        // recebe os dados do cliente
         int recv_count =
             recvfrom(s, clientInfo, sizeof(*clientInfo), 0,
                      (struct sockaddr *)(&clientInfo->storage), &caddrlen);
         if (recv_count == -1) {
             logexit("recvfrom");
         }
-
+        // Cria uma nova thread para lidar com as informações recebidas do
+        // cliente.
         pthread_t tid;
         pthread_create(&tid, NULL, client_thread, clientInfo);
+        // Desanexa a nova thread para liberar seus recursos automaticamente
+        // após a conclusão.
         pthread_detach(tid);
     }
-
+    // fecha o socket
     close(s);
     return 0;
 }
